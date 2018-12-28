bc09378082372bd9c62bf1f45ee87b0f1a7eb566
Simon Galasso <sgalasso@student.42.fr>
2018-12-28 17:34:45 +0100

    sgalasso : archi code
    modifs set string

--- srcs/ft_set_infos.c
+++ srcs/ft_set_infos.c
@@ -6,7 +6,7 @@
 /*   By: sgalasso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 13:41:45 by sgalasso          #+#    #+#             */
-/*   Updated: 2018/12/28 16:40:51 by sgalasso         ###   ########.fr       */
+/*   Updated: 2018/12/28 17:31:44 by sgalasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,127 +21,88 @@ SDL_Color	ft_set_color(t_data *data, int i)
 	return (ft_hex_to_rgb(H_GREEN));
 }
 
+void	ft_set_nbrstring(int value, SDL_Rect rect, Uint32 color, t_data *data)
+{
+	char	*str;
+	
+	if (!(str = ft_itoa(value)))
+		ft_err_exit("wolf3d: error: itoa, out of memory", data);
+	ft_set_string(rect, str, ft_hex_to_rgb(color), data);
+	lt_release(str);
+}
+
 void	ft_set_menu_config(t_data *data)
 {
 	SDL_Rect	rect;
-	char		*speed_str;
-	char		*sens_str;
-	char		*light_str;
-	char		*txtr_str;
-	char		*zoom_str;
-
-	if (!(speed_str = ft_itoa((int)(data->player.speed * 100))))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-	if (!(sens_str = ft_itoa(data->player.sensibility)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-	if (!(light_str = ft_itoa(data->lightshade)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-	if (!(txtr_str = ft_itoa(data->texturing)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-	if (!(zoom_str = ft_itoa(data->minimap.mnp_size)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
 
 	rect = (SDL_Rect){230,240,500,35};
 	ft_set_string(rect, "SETTINGS", ft_hex_to_rgb(H_GREEN), data);
-
 	rect = (SDL_Rect){230,300,500,25};
 	ft_set_string(rect, "[ 8 9 ]  Speed", ft_hex_to_rgb(H_GREEN), data);
 	rect = (SDL_Rect){500,300,500,25};
-	ft_set_string(rect, speed_str, ft_hex_to_rgb(H_GREEN), data);
-
+	ft_set_nbrstring((int)(data->player.speed * 100), rect, H_GREEN, data);
 	rect = (SDL_Rect){230,340,500,25};
 	ft_set_string(rect, "[ * / ]  Sensibility", ft_hex_to_rgb(H_GREEN), data);
 	rect = (SDL_Rect){500,340,500,25};
-	ft_set_string(rect, sens_str, ft_hex_to_rgb(H_GREEN), data);
-
+	ft_set_nbrstring(data->player.sensibility, rect, H_GREEN, data);
 	rect = (SDL_Rect){230,380,500,25};
 	ft_set_string(rect, "[ l ]  Light shading", ft_hex_to_rgb(H_GREEN), data);
 	rect = (SDL_Rect){500,380,500,25};
-	ft_set_string(rect, light_str, ft_hex_to_rgb(H_GREEN), data);
-
+	ft_set_nbrstring(data->lightshade, rect, H_GREEN, data);
 	rect = (SDL_Rect){230,420,500,25};
 	ft_set_string(rect, "[ t ]  Texturing", ft_hex_to_rgb(H_GREEN), data);
 	rect = (SDL_Rect){500,420,500,25};
-	ft_set_string(rect, txtr_str, ft_hex_to_rgb(H_GREEN), data);
-
+	ft_set_nbrstring(data->texturing, rect, H_GREEN, data);
 	rect = (SDL_Rect){230,460,500,25};
 	ft_set_string(rect, "[ + - ]  Minimap zoom", ft_hex_to_rgb(H_GREEN), data);
 	rect = (SDL_Rect){500,460,500,25};
-	ft_set_string(rect, zoom_str, ft_hex_to_rgb(H_GREEN), data);
-
+	ft_set_nbrstring(data->minimap.mnp_size, rect, H_GREEN, data);
 	rect = (SDL_Rect){230,500,500,25};
 	ft_set_string(rect, "[ Esc ]  Exit", ft_hex_to_rgb(H_GREEN), data);
-
-	lt_release(speed_str);
-	lt_release(sens_str);
-	lt_release(light_str);
-	lt_release(txtr_str);
-	lt_release(zoom_str);
 }
 
 void	ft_dev_mode(t_data *data)
 {
 	SDL_Rect	rect;
-	char		*fps_str;
-	char		*frame_str;
-	char		*posx_str;
-	char		*posy_str;
-	char		*dir_str;
 
-	if (!(fps_str = ft_itoa(data->fps)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-	if (!(frame_str = ft_itoa(data->nb_frame)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-	if (!(posx_str = ft_itoa(data->player.position.x * BLOC_SIZE)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-	if (!(posy_str = ft_itoa(data->player.position.y * BLOC_SIZE)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-	if (!(dir_str = ft_itoa(data->player.direction)))
-		ft_err_exit("wolf3d: error: itoa, out of memory", data);
-
-	rect = (SDL_Rect){20,18,150,15};
+	rect = (SDL_Rect){20, 18, 150, 15};
 	ft_set_string(rect, "1 normal", ft_set_color(data, 0), data);
-	rect = (SDL_Rect){20,50,150,30};
+	rect = (SDL_Rect){20, 50, 150, 30};
 	ft_set_string(rect, "fps : ", ft_hex_to_rgb(H_GREEN), data);
-	rect = (SDL_Rect){80,50,150,30};
-	ft_set_string(rect, fps_str, ft_hex_to_rgb(H_GREEN), data);
-	rect = (SDL_Rect){160,50,150,30};
-	ft_set_string(rect, frame_str, ft_hex_to_rgb(H_GREEN), data);
-
-	rect = (SDL_Rect){20,100,150,30};
+	rect = (SDL_Rect){160, 50, 150, 30};
+	ft_set_nbrstring(data->fps, rect, H_GREEN, data);
+	rect = (SDL_Rect){20, 100, 150, 30};
+	ft_set_string(rect, "frames : ", ft_hex_to_rgb(H_GREEN), data);
+	rect = (SDL_Rect){160, 100, 150, 30};
+	ft_set_nbrstring(data->nb_frame, rect, H_GREEN, data);
+	rect = (SDL_Rect){20, 140, 150, 30};
 	ft_set_string(rect, "position.x : ", ft_hex_to_rgb(H_GREEN), data);
-	rect = (SDL_Rect){160,100,150,30};
-	ft_set_string(rect, posx_str, ft_hex_to_rgb(H_GREEN), data);
-
-	rect = (SDL_Rect){20,140,150,30};
+	rect = (SDL_Rect){160, 140, 150, 30};
+	ft_set_nbrstring(data->player.position.x * BLOC_SIZE, rect, H_GREEN, data);
+	rect = (SDL_Rect){20, 180, 150, 30};
 	ft_set_string(rect, "position.y : ", ft_hex_to_rgb(H_GREEN), data);
-	rect = (SDL_Rect){160,140,150,30};
-	ft_set_string(rect, posy_str, ft_hex_to_rgb(H_GREEN), data);
-
-	rect = (SDL_Rect){20,180,150,30};
+	rect = (SDL_Rect){160, 180, 150, 30};
+	ft_set_nbrstring(data->player.position.y * BLOC_SIZE, rect, H_GREEN, data);
+	rect = (SDL_Rect){20, 220, 150, 30};
 	ft_set_string(rect, "direction : ", ft_hex_to_rgb(H_GREEN), data);
-	rect = (SDL_Rect){160,180,150,30};
-	ft_set_string(rect, dir_str, ft_hex_to_rgb(H_GREEN), data);
-	
-	lt_release(fps_str);
-	lt_release(frame_str);
-	lt_release(posx_str);
-	lt_release(posy_str);
-	lt_release(dir_str);
+	rect = (SDL_Rect){160, 220, 150, 30};
+	ft_set_nbrstring(data->player.direction, rect, H_GREEN, data);
 }
 
 void	ft_set_infos(t_data *data)
 {
 	SDL_Rect	rect;
 
-	rect = (SDL_Rect){20,18,150,15};
+	rect = (SDL_Rect){20, 18, 150, 15};
 	ft_set_string(rect, "1 normal", ft_set_color(data, 0), data);
-	rect = (SDL_Rect){87,18,150,15};
+	rect = (SDL_Rect){87, 18, 150, 15};
 	ft_set_string(rect, "2 gaming", ft_set_color(data, 1), data);
+	rect = (SDL_Rect){184, 18, 150, 15};
+	ft_set_string(rect, "3 dev", ft_set_color(data, 2), data);
+
 	rect = (SDL_Rect){WIN_W / 2 - 75,660,150,20};
 	ft_set_string(rect, "SETTINGS : [ i ]", ft_set_color(data, 3), data);
-	rect = (SDL_Rect){184,18,150,15};
-	ft_set_string(rect, "3 dev", ft_set_color(data, 2), data);
+
 	if (data->setting == 1)
 		ft_set_menu_config(data);
 	else if (data->dev_mode == 1)
--- srcs/ft_set_interface.c
+++ srcs/ft_set_interface.c
@@ -6,7 +6,7 @@
 /*   By: sgalasso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 16:46:59 by sgalasso          #+#    #+#             */
-/*   Updated: 2018/12/28 16:41:02 by sgalasso         ###   ########.fr       */
+/*   Updated: 2018/12/28 17:25:44 by sgalasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_set_config(t_data *data)
 {
 	SDL_Rect	rect;
 
-	rect = (SDL_Rect){200,200,560,372};
+	rect = (SDL_Rect){200, 200, 560, 372};
 	ft_draw_rect(rect,  0x10000000, 0, data);
 	ft_draw_border(rect, 0xFFFFFFFF, data);
 }
@@ -25,16 +25,15 @@ void	ft_bloc_devmode(t_data *data)
 {
 	SDL_Rect	rect;
 
-	rect = (SDL_Rect){15,48,200,35};
+	rect = (SDL_Rect){15, 48, 220, 35};
 	ft_draw_rect(rect, 0x10000000, 0, data);
-
-	rect = (SDL_Rect){15,98,200,35};
+	rect = (SDL_Rect){15, 98, 220, 35};
 	ft_draw_rect(rect, 0x10000000, 0, data);
-
-	rect = (SDL_Rect){15,138,200,35};
+	rect = (SDL_Rect){15, 138, 220, 35};
 	ft_draw_rect(rect, 0x10000000, 0, data);
-
-	rect = (SDL_Rect){15,178,200,35};
+	rect = (SDL_Rect){15, 178, 220, 35};
+	ft_draw_rect(rect, 0x10000000, 0, data);
+	rect = (SDL_Rect){15, 218, 220, 35};
 	ft_draw_rect(rect, 0x10000000, 0, data);
 }
 
@@ -44,10 +43,10 @@ void	ft_set_interface(t_data *data)
 
 	if (data->gamemode == 1 || data->dev_mode == 1)
 		ft_minimap(data);
-	rect = (SDL_Rect){15,15,138,20};
+	rect = (SDL_Rect){15, 15, 138, 20};
 	ft_draw_rect(rect,  0x00000000, 0, data);
 	ft_draw_border(rect, 0xFFFFFFFF, data);
-	rect = (SDL_Rect){170,15,60,20};
+	rect = (SDL_Rect){170, 15, 60, 20};
 	ft_draw_rect(rect, 0xFF000000, 0, data);
 	ft_draw_border(rect, 0xFFFFFFFF, data);
 	if (data->setting == 1)
